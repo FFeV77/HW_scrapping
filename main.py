4946919cfc960ad9db9df5c3180c85bfcf527650
Andrey <bucksbunny.butovo@gmail.com>
2022-09-05 17:54:22 +0300

    matches()

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,15 +6,23 @@ URL = 'https://habr.com'
 KEYWORDS = ['биткоин']
 HEADERS = {...}
 
+def matches(hubs, header, body, time, href):
+    '''Поиск совпадений с ключевыми словами (KEYWORDS)'''
+    for keyword in KEYWORDS:
+        if (keyword in hubs) or (header.find(keyword) != -1) or (body.find(keyword) != -1):
+            print(f'{time} - {header} - {URL + href}')
+    return
+
+
 def page_soup(page_link):
     base_url = URL + page_link
     data = requests.get(base_url, headers=HEADERS)
     page = data.text
-
     soup = BeautifulSoup(page, "html5lib")
     return soup
 
-def search_on_main(articles):
+
+def scrap_main(articles):
     for article in articles:
         header = article.h2.text
         href = article.h2.a['href']
@@ -21,12 +30,11 @@ def search_on_main(articles):
         body = article.find('div', class_ = 'tm-article-body tm-article-snippet__lead').text
         hubs = article.find_all(class_ = 'tm-article-snippet__hubs-item')
         hubs = [hub.text.strip() for hub in hubs]
-        for keyword in KEYWORDS:
-            if (keyword in hubs) or (header.find(keyword) != -1) or (body.find(keyword) != -1):
-                print(f'{time} - {header} - {URL + href}')
+        matches(hubs, header, body, time, href)
     return
 
-def search_on_article(articles):
+
+def scrap_article(articles):
     for article in articles:
         href = article.h2.a['href']
         soup = page_soup(href)
@@ -36,14 +44,12 @@ def search_on_article(articles):
         hubs = soup.find_all(class_ = 'tm-separated-list__item')
         hubs = [hub.text.strip() for hub in hubs]
         body = soup.find('div', class_ = 'tm-article-body').text
-        for keyword in KEYWORDS:
-            if (keyword in hubs) or (header.find(keyword) != -1) or (body.find(keyword) != -1):
-                print(f'{time} - {header} - {URL + href}')
-
+        matches(hubs, header, body, time, href)
     return
 
-main_page = '/ru/all/'
-soup = page_soup(main_page)
-articles = soup.find_all('article')
-# search_on_main(articles)
-search_on_article(articles)
+if __name__ == '__main__':
+    main_page = '/ru/all/'
+    soup = page_soup(main_page)
+    articles = soup.find_all('article')
+    # scrap_main(articles)
+    scrap_article(articles)
